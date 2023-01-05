11e650d5f479ab694dacb7a4af80d9a1e7c2c4bd
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2023-01-05 17:17:06 -0500

    Update Main.java
    YAY it drives!!! (although something is wrong with the forward and backward movement)

--- Main.java
+++ Main.java
@@ -21,35 +21,82 @@ telemetry.addData("Status", "Running");
 telemetry.update();
 */
 
-@TeleOp
-public class MyFIRSTJavaOpMode extends LinearOpMode {
-    private Gyroscope imu;
-    private DcMotor motorTest;
-    private DigitalChannel digitalTouch;
-    private DistanceSensor sensorColorRange;
-    private Servo servoTest;
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-    @Override
-    public void runOpMode() {
-        imu = hardwareMap.get(Gyroscope.class, "imu");
-        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
-        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
-        servoTest = hardwareMap.get(Servo.class, "servoTest");
+@TeleOp(name = "Run1 (Blocks to Java)")
+public class Run1 extends LinearOpMode {
 
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
+  private DcMotor Backleft;
+  private DcMotor Backright;
+  private DcMotor Frontleft;
+  private DcMotor Frontright;
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            telemetry.addData("Status", "Running");
-            telemetry.update();
+  /**
+   * This function is executed when this Op Mode is selected from the Driver Station.
+   */
+  @Override
+  public void runOpMode() {
+    Backleft = hardwareMap.get(DcMotor.class, "Backleft");
+    Backright = hardwareMap.get(DcMotor.class, "Backright");
+    Frontleft = hardwareMap.get(DcMotor.class, "Frontleft");
+    Frontright = hardwareMap.get(DcMotor.class, "Frontright");
 
+    telemetry.addData("Status", "Initialized");
+    waitForStart();
+    if (opModeIsActive()) {
+      while (opModeIsActive()) {
+        while (gamepad1.dpad_left) {
+          left();
         }
+        while (gamepad1.dpad_right) {
+          right();
+        }
+        while (gamepad1.dpad_up) {
+          forward();
+        }
+      }
+      telemetry.addData("Status", "Running");
+      telemetry.update();
     }
+  }
+
+  private void left() {
+    Backleft.setDirection(DcMotorSimple.Direction.FORWARD);
+    Backleft.setPower(0.2);
+    Backright.setDirection(DcMotorSimple.Direction.FORWARD);
+    Backright.setPower(0.2);
+    Frontleft.setDirection(DcMotorSimple.Direction.REVERSE);
+    Frontleft.setPower(0.2);
+    Frontright.setDirection(DcMotorSimple.Direction.REVERSE);
+    Frontright.setPower(0.2);
+  }
+
+  private void right() {
+    Backleft.setDirection(DcMotorSimple.Direction.REVERSE);
+    Backleft.setPower(0.2);
+    Backright.setDirection(DcMotorSimple.Direction.REVERSE);
+    Backright.setPower(0.2);
+    Frontleft.setDirection(DcMotorSimple.Direction.FORWARD);
+    Frontleft.setPower(0.2);
+    Frontright.setDirection(DcMotorSimple.Direction.FORWARD);
+    Frontright.setPower(0.2);
+  }
+
+  private void forward() {
+    Backleft.setDirection(DcMotorSimple.Direction.REVERSE);
+    Backleft.setPower(0.2);
+    Backright.setDirection(DcMotorSimple.Direction.REVERSE);
+    Backright.setPower(0.2);
+    Frontleft.setDirection(DcMotorSimple.Direction.FORWARD);
+    Frontleft.setPower(0.2);
+    Frontright.setDirection(DcMotorSimple.Direction.FORWARD);
+    Frontright.setPower(0.2);
+  }
 }
 
 // run until the end of the match (driver presses STOP)
