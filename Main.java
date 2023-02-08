dd3c0be123a4bf7ea3fa68b6f81ac1f7f1f94a57
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2023-02-08 12:23:40 -0500

    NEW CODE!!!
    many changes to come,. this has basic lift movement, driving

--- Main.java
+++ Main.java
@@ -20,7 +20,6 @@ if (digitalTouch.getState() == false) {
 telemetry.addData("Status", "Running");
 telemetry.update();
 */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -28,60 +27,101 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "Run1 (Blocks to Java)")
-public class Run1 extends LinearOpMode {
+@TeleOp(name = "_5276OpMode2 (Blocks to Java)")
+public class _5276OpMode2 extends LinearOpMode {
 
-  private CRServo Hand;
-  private DcMotor Backleft;
-  private DcMotor Backright;
-  private DcMotor Frontleft;
-  private DcMotor Frontright;
+  private DcMotor lift;
+  private DcMotor span;
+  private Servo testcontrol;
+  private CRServo test;
+  private DcMotor frontright;
+  private DcMotor frontleft;
+  private DcMotor backright;
+  private DcMotor backleft;
 
   /**
    * This function is executed when this Op Mode is selected from the Driver Station.
    */
   @Override
   public void runOpMode() {
-    Hand = hardwareMap.get(CRServo.class, "Hand");
-    Backleft = hardwareMap.get(DcMotor.class, "Backleft");
-    Backright = hardwareMap.get(DcMotor.class, "Backright");
-    Frontleft = hardwareMap.get(DcMotor.class, "Frontleft");
-    Frontright = hardwareMap.get(DcMotor.class, "Frontright");
+    lift = hardwareMap.get(DcMotor.class, "lift");
+    span = hardwareMap.get(DcMotor.class, "span");
+    testcontrol = hardwareMap.get(Servo.class, "testcontrol");
+    test = hardwareMap.get(CRServo.class, "test");
+    frontright = hardwareMap.get(DcMotor.class, "frontright");
+    frontleft = hardwareMap.get(DcMotor.class, "frontleft");
+    backright = hardwareMap.get(DcMotor.class, "backright");
+    backleft = hardwareMap.get(DcMotor.class, "backleft");
 
     // Put initialization blocks here.
-    telemetry.addData("Status", "Initialized");
+    lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     waitForStart();
     if (opModeIsActive()) {
+      // Put run blocks here.
       while (opModeIsActive()) {
         if (gamepad1.dpad_up) {
+          set_power();
           forward();
         } else {
           if (gamepad1.dpad_down) {
-            backward();
+            set_power();
+            reverse();
           } else {
             if (gamepad1.dpad_right) {
+              set_power();
               right();
             } else {
               if (gamepad1.dpad_left) {
+                set_power();
                 left();
               } else {
-                wait();
+                if (gamepad1.x) {
+                  set_power();
+                  ccw();
+                } else {
+                  if (gamepad1.b) {
+                    set_power();
+                    cw();
+                  } else {
+                    stop2();
+                  }
+                }
               }
             }
           }
         }
-        if (gamepad1.a) {
-          grab();
+        span.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        if (gamepad2.dpad_up) {
+          out();
         } else {
-          if (gamepad1.b) {
-            release();
+          if (gamepad2.dpad_down) {
+            in();
           } else {
-            Hand.setPower(0);
+            span.setPower(0);
           }
         }
-        telemetry.addData("Status", "Running");
+        if (gamepad2.left_bumper) {
+          up();
+        } else {
+          if (gamepad2.right_bumper) {
+            down();
+          } else {
+            lift.setPower(0);
+          }
+        }
+        if (gamepad1.y) {
+          testcontrol.setPosition(testcontrol.getPosition() + 1);
+        } else {
+          testcontrol.setPosition(testcontrol.getPosition());
+        }
+        if (gamepad1.a) {
+          test.setDirection(DcMotorSimple.Direction.FORWARD);
+          test.setPower(0.1);
+        } else {
+          test.setPower(0);
+        }
         telemetry.update();
       }
     }
@@ -90,113 +130,116 @@ public class Run1 extends LinearOpMode {
   /**
    * Describe this function...
    */
-  private void left() {
-    Backleft.setDirection(DcMotorSimple.Direction.REVERSE);
-    Backleft.setPower(0.2);
-    Backright.setDirection(DcMotorSimple.Direction.FORWARD);
-    Backright.setPower(0.2);
-    Frontleft.setDirection(DcMotorSimple.Direction.FORWARD);
-    Frontleft.setPower(0.2);
-    Frontright.setDirection(DcMotorSimple.Direction.REVERSE);
-    Frontright.setPower(0.2);
+  private void set_power() {
+    frontright.setPower(0.7);
+    frontleft.setPower(0.7);
+    backright.setPower(0.7);
+    backleft.setPower(0.7);
   }
 
   /**
    * Describe this function...
    */
-  private void right() {
-    Backleft.setDirection(DcMotorSimple.Direction.FORWARD);
-    Backleft.setPower(0.2);
-    Backright.setDirection(DcMotorSimple.Direction.REVERSE);
-    Backright.setPower(0.2);
-    Frontleft.setDirection(DcMotorSimple.Direction.REVERSE);
-    Frontleft.setPower(0.2);
-    Frontright.setDirection(DcMotorSimple.Direction.FORWARD);
-    Frontright.setPower(0.2);
+  private void stop2() {
+    frontright.setPower(0);
+    frontleft.setPower(0);
+    backright.setPower(0);
+    backleft.setPower(0);
   }
 
   /**
    * Describe this function...
    */
   private void forward() {
-    Backleft.setDirection(DcMotorSimple.Direction.FORWARD);
-    Backleft.setPower(0.2);
-    Backright.setDirection(DcMotorSimple.Direction.FORWARD);
-    Backright.setPower(0.2);
-    Frontleft.setDirection(DcMotorSimple.Direction.FORWARD);
-    Frontleft.setPower(0.2);
-    Frontright.setDirection(DcMotorSimple.Direction.FORWARD);
-    Frontright.setPower(0.2);
+    frontright.setDirection(DcMotorSimple.Direction.FORWARD);
+    frontleft.setDirection(DcMotorSimple.Direction.REVERSE);
+    backright.setDirection(DcMotorSimple.Direction.FORWARD);
+    backleft.setDirection(DcMotorSimple.Direction.REVERSE);
   }
 
   /**
    * Describe this function...
    */
-  private void backward() {
-    Backleft.setDirection(DcMotorSimple.Direction.REVERSE);
-    Backleft.setPower(0.2);
-    Backright.setDirection(DcMotorSimple.Direction.REVERSE);
-    Backright.setPower(0.2);
-    Frontleft.setDirection(DcMotorSimple.Direction.REVERSE);
-    Frontleft.setPower(0.2);
-    Frontright.setDirection(DcMotorSimple.Direction.REVERSE);
-    Frontright.setPower(0.2);
+  private void reverse() {
+    frontright.setDirection(DcMotorSimple.Direction.REVERSE);
+    frontleft.setDirection(DcMotorSimple.Direction.FORWARD);
+    backright.setDirection(DcMotorSimple.Direction.REVERSE);
+    backleft.setDirection(DcMotorSimple.Direction.FORWARD);
   }
 
   /**
    * Describe this function...
    */
-  private void wait() {
-    Backleft.setPower(0);
-    Backright.setPower(0);
-    Frontleft.setPower(0);
-    Frontright.setPower(0);
+  private void right() {
+    frontright.setDirection(DcMotorSimple.Direction.FORWARD);
+    frontleft.setDirection(DcMotorSimple.Direction.REVERSE);
+    backright.setDirection(DcMotorSimple.Direction.REVERSE);
+    backleft.setDirection(DcMotorSimple.Direction.FORWARD);
   }
-    
-  /**
-  * Describe this function...
-  */
-  private void grab() {
-      // 180 degrees
-    Hand.setPosition(1);
-//     Hand.setDirection(DcMotorSimple.Direction.FORWARD);
-//     Hand.setPower(0.1);
-      
-  }
-   
+
   /**
    * Describe this function...
    */
-  private void release() {
-      // 0 degrees
-      Hand.setPosition(0);
-//     Hand.setDirection(DcMotorSimple.Direction.REVERSE);
-//     Hand.setPower(0.1);
+  private void left() {
+    frontright.setDirection(DcMotorSimple.Direction.REVERSE);
+    frontleft.setDirection(DcMotorSimple.Direction.FORWARD);
+    backright.setDirection(DcMotorSimple.Direction.FORWARD);
+    backleft.setDirection(DcMotorSimple.Direction.REVERSE);
+  }
+
+  /**
+   * Describe this function...
+   */
+  private void cw() {
+    frontright.setDirection(DcMotorSimple.Direction.REVERSE);
+    frontleft.setDirection(DcMotorSimple.Direction.REVERSE);
+    backright.setDirection(DcMotorSimple.Direction.REVERSE);
+    backleft.setDirection(DcMotorSimple.Direction.REVERSE);
+  }
+
+  /**
+   * Describe this function...
+   */
+  private void ccw() {
+    frontright.setDirection(DcMotorSimple.Direction.FORWARD);
+    frontleft.setDirection(DcMotorSimple.Direction.FORWARD);
+    backright.setDirection(DcMotorSimple.Direction.FORWARD);
+    backleft.setDirection(DcMotorSimple.Direction.FORWARD);
+  }
+
+  /**
+   * Describe this function...
+   */
+  private void in() {
+    span.setPower(0.5);
+    span.setDirection(DcMotorSimple.Direction.REVERSE);
+  }
+
+  /**
+   * Describe this function...
+   */
+  private void out() {
+    span.setPower(0.5);
+    span.setDirection(DcMotorSimple.Direction.FORWARD);
+  }
+
+  /**
+   * Describe this function...
+   */
+  private void up() {
+    lift.setPower(0.3);
+    lift.setTargetPosition(backleft.getCurrentPosition() - 780);
+    lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+  }
+
+  /**
+   * Describe this function...
+   */
+  private void down() {
+    lift.setPower(0.3);
+    lift.setTargetPosition(backleft.getCurrentPosition() - 1);
+    lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
   }
 }
-
-//control a servo
-// run until the end of the match (driver presses STOP)
-double tgtPower = 0;
-while (opModeIsActive()) {
-    tgtPower = -this.gamepad1.left_stick_y;
-    motorTest.setPower(tgtPower);
-    // check to see if we need to move the servo.
-    if(gamepad1.y) {
-        // move to 0 degrees.
-        servoTest.setPosition(0);
-    } else if (gamepad1.x || gamepad1.b) {
-        // move to 90 degrees.
-        servoTest.setPosition(0.5);
-    } else if (gamepad1.a) {
-        // move to 180 degrees.
-        servoTest.setPosition(1);
-    }
-    telemetry.addData("Servo Position", servoTest.getPosition());
-    telemetry.addData("Target Power", tgtPower);
-    telemetry.addData("Motor Power", motorTest.getPower());
-    telemetry.addData("Status", "Running");
-    telemetry.update();
-
-}
-
